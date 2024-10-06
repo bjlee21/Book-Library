82df0f3766324bf65c527830048520e6d4d94484
bjlee21 <brandonjlee21@gmail.com>
2024-10-06 14:06:54 -0500

    First Commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://brandonjlee21:Golf1234@cluster0.h8bey.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
