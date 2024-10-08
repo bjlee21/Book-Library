f845b1e733761d17a9042e15d6414bb58b925b25
bjlee21 <brandonjlee21@gmail.com>
2024-10-08 17:38:11 -0500

    fix

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://brandonjlee21:Golf1234@cluster0.h8bey.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://brandonjlee21:fUHkWo5RYSeBTw8o@cluster0.h8bey.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
