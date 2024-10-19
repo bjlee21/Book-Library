35f3d4014df9b4333b3726d4fb49e6f24d6af5fe
bjlee21 <brandonjlee21@gmail.com>
2024-10-19 01:46:56 -0500

    Initial commit

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT = 5555;
 
-export const mongoDBURL = 
-    'mongodb+srv://brandonjlee21:fUHkWo5RYSeBTw8o@cluster0.h8bey.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+export const mongoDBURL = process.env.MONGODB_URL;
